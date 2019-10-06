482022dbd4569e8bcabe26e995a3356898b61e66
Giles Hinchcliff <giles@rebelastronaut.co.uk>
2019-10-06 15:14:19 +0100

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,19 +78,44 @@ module.exports = {
             },
           },
           `gatsby-remark-images-zoom`,
+          {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+              logo: "./src/favicon.png",
+
+              // WebApp Manifest Configuration
+              appName: null, // Inferred with your package.json
+              appDescription: null,
+              developerName: null,
+              developerURL: null,
+              dir: 'auto',
+              lang: 'en-US',
+              background: '#fff',
+              theme_color: '#fff',
+              display: 'standalone',
+              orientation: 'any',
+              start_url: '/?homescreen=1',
+              version: '1.0',
+
+              icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                yandex: false,
+                windows: false
+              }
+            }
+          }
         ],
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/favicon.png`,
       },
     },
     `gatsby-plugin-netlify-cms`,  
@@ -104,6 +129,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
